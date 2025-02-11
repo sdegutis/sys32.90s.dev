b26a5ac888ecd156de5a231ade3ea71b43f20fc3
sdegutis <sbdegutis@gmail.com>
2025-02-11 15:54:14 -0600

    Fix bug.

--- site/font.ts
+++ site/font.ts
@@ -2,26 +2,26 @@ import { COLORS, ctx } from "./crt.js";
 
 const mapping = `abcdefghijklmnopqrstuvwxyz .,'!?1234567890-+/()":;%*=[]<>_`;
 const src = `
- xxx     xx      xxx     xx      xxx     xxx     xxx     x x     xxx     xxx     x x     x       xxx     xxx     xxx     xxx   |
- x x     xxx     x       x x     xx      xx      x       xxx      x       x      xx      x       xxx     x x     x x     x x   |
- xxx     x x     x       x x     x       x       x x     x x      x       x      xx      x       x x     x x     x x     xx    |
- x x     xxx     xxx     xx      xxx     x       xxx     x x     xxx     xx      x x     xxx     x x     x x     xxx     x     |
-                                                                                                                               |
- xxx     xxx     xxx     xxx     x x     x x     x x     x x     x x     xxx                             xx       x      xxx   |
- x x     x x     x        x      x x     x x     x x      x      x x      xx                              x       x      x x   |
- xxx     xx       xx      x      x x     x x     xxx      x       x      x                        x                            |
-   x     x x     xxx      x      xxx      x      xxx     x x      x      xxx              x      xx               x        x   |
-                                                                                                                               |
- xx      xx      xxx     x x     xxx     xxx     xxx     xxx     xxx      x               x        x       x      x      x x   |
-  x        x      xx     x x     xx      x         x     xxx     x x     x x     xxx     xxx      x       x        x     x x   |
-  x       x        x     xxx       x     xxx       x     x x      xx     x x              x       x       x        x           |
- xxx     xxx     xxx       x     xx      xxx       x     xxx     xx       x                      x         x      x            |
-                                                                                                                               |
- x        x       x      x x     xx      xx      xx       x      x                                                             |
-                          x              x        x      x        x                                                            |
- x        x      xxx     x x     xx      x        x       x      x                                                             |
-         x        x                      xx      xx                      xxx                                                   |
-`.replaceAll(/\r?\n/g, '');
+ xxx     xx      xxx     xx      xxx     xxx     xxx     x x     xxx     xxx     x x     x       xxx     xxx     xxx     xxx  |
+ x x     xxx     x       x x     xx      xx      x       xxx      x       x      xx      x       xxx     x x     x x     x x  |
+ xxx     x x     x       x x     x       x       x x     x x      x       x      xx      x       x x     x x     x x     xx   |
+ x x     xxx     xxx     xx      xxx     x       xxx     x x     xxx     xx      x x     xxx     x x     x x     xxx     x    |
+                                                                                                                              |
+ xxx     xxx     xxx     xxx     x x     x x     x x     x x     x x     xxx                             xx       x      xxx  |
+ x x     x x     x        x      x x     x x     x x      x      x x      xx                              x       x      x x  |
+ xxx     xx       xx      x      x x     x x     xxx      x       x      x                        x                           |
+   x     x x     xxx      x      xxx      x      xxx     x x      x      xxx              x      xx               x        x  |
+                                                                                                                              |
+ xx      xx      xxx     x x     xxx     xxx     xxx     xxx     xxx      x               x        x       x      x      x x  |
+  x        x      xx     x x     xx      x         x     xxx     x x     x x     xxx     xxx      x       x        x     x x  |
+  x       x        x     xxx       x     xxx       x     x x      xx     x x              x       x       x        x          |
+ xxx     xxx     xxx       x     xx      xxx       x     xxx     xx       x                      x         x      x           |
+                                                                                                                              |
+ x        x       x      x x     xx      xx      xx       x      x                                                            |
+                          x              x        x      x        x                                                           |
+ x        x      xxx     x x     xx      x        x       x      x                                                            |
+         x        x                      xx      xx                      xxx                                                  |`;
+
 const chars: Record<string, boolean[][]> = {};
 
 for (let i = 0; i < mapping.length; i++) {
@@ -37,7 +37,7 @@ for (let i = 0; i < mapping.length; i++) {
     for (let x = 0; x < 4; x++) {
       const px = (i % 16) * 8 + 1 + x;
       const py = (Math.floor(i / 16) * 5) + y;
-      const index = (px + py * 16 * 8);
+      const index = 1 + (px + py * 16 * 8);
 
       row.push(src[index] === ' ' ? false : true);
     }
