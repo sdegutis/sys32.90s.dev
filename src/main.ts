4683c236decaf5107867a4c826f892ba4f1beb89
sdegutis <sbdegutis@gmail.com>
2025-02-11 06:08:54 -0600

    Not quite right...

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { canvas, COLORS, ctx, openCRT } from "./crt";
 
-const crt = openCRT();
-
 type Tile = {
   type: 'grass' | 'tree' | 'farmer',
   x: number,
@@ -34,36 +32,41 @@ tiles[100][100].type = 'farmer';
 
 let drag: { x: number, y: number } | null = null;
 
-function maybeGrow(x: number, y: number) {
-  if (x < 0 || y < 0 || x >= 320 || y >= 180) return;
-  if (tiles[y][x].type === 'grass') {
-    if (Math.random() > 0.25) {
-      tiles[y][x].type = 'tree';
-      tiles[y][x].age = 0;
-    }
-  }
-}
-
+const crt = openCRT();
 crt.update = (t: number, delta: number) => {
   // maybe grow trees
   for (let y = 0; y < 180; y++) {
     for (let x = 0; x < 320; x++) {
       const tile = tiles[y][x];
       if (tile.type === 'tree') {
-        if (tile.age > 1000) {
+        if (tile.age > 10) {
+          tile.age = 0;
 
-          for (let xx = -1; xx <= 1; xx += 2) {
-            for (let yy = -1; yy <= 1; yy += 2) {
-              maybeGrow(x + xx, y + yy);
+          for (let yy = 0; yy < 3; yy += 2) {
+            for (let xx = 0; xx < 3; xx += 2) {
+              const x2 = x + xx - 1;
+              const y2 = y + yy - 1;
+
+              if (x2 >= 0 && y2 >= 0 && x2 < 320 && y2 < 180) {
+                const nei = tiles[y2][x2];
+
+                if (nei.type === 'grass') {
+                  if (Math.random() > 0.01) {
+                    nei.type = 'tree';
+                    nei.age = 0;
+                  }
+                }
+              }
+
             }
           }
-
         }
         tile.age += delta;
       }
     }
   }
 
+  // draw each tile
   for (let y = 0; y < 180; y++) {
     for (let x = 0; x < 320; x++) {
       ctx.fillStyle = colors[tiles[y][x].type];
@@ -71,6 +74,7 @@ crt.update = (t: number, delta: number) => {
     }
   }
 
+  // draw selection
   if (drag) {
     ctx.strokeStyle = '#00f3';
     ctx.strokeRect(
@@ -90,7 +94,8 @@ crt.update = (t: number, delta: number) => {
 
 canvas.onmousedown = () => {
   drag = { x: crt.mouse.x, y: crt.mouse.y };
-  canvas.onmouseup = () => {
-    drag = null;
-  };
+};
+
+canvas.onmouseup = () => {
+  drag = null;
 };
