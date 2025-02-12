d8496e67b5c72cc0e88125e082deb84313ce10f7
sdegutis <sbdegutis@gmail.com>
2025-02-12 04:33:47 -0600

    Start mapmaker.

---
+++ site/mapmaker.ts
@@ -0,0 +1,105 @@
+import { callbacks, COLORS, context, mouse } from "./crt.js";
+
+type Tile = {
+  type: 'grass' | 'tree' | 'farmer' | 'gold' | 'rock',
+  x: number,
+  y: number,
+  age: number,
+};
+
+// 80 x 45
+
+const tiles: Tile[][] = [];
+for (let y = 0; y < 45; y++) {
+  const row: Tile[] = [];
+  tiles.push(row);
+  for (let x = 0; x < 80; x++) {
+    row.push({ type: 'grass', x, y, age: 0 });
+  }
+}
+
+function pset(c: number, x: number, y: number) {
+  context.fillStyle = COLORS[c];
+  context.fillRect(x, y, 1, 1);
+}
+
+const draw: { [K in Tile['type']]: (x: number, y: number) => void } = {
+  grass: (x, y) => {
+    context.fillStyle = COLORS[3];
+    context.fillRect(x, y, 4, 4);
+  },
+  gold: (x, y) => {
+    // draw.grass(x, y);
+    pset(6, x + 2, y + 1);
+    pset(6, x + 1, y + 2);
+    pset(6, x + 1, y + 3);
+    pset(5, x + 3, y + 2);
+    pset(5, x + 3, y + 3);
+    pset(1, x + 2, y + 2);
+    pset(1, x + 2, y + 3);
+  },
+  rock: (x, y) => {
+    draw.grass(x, y);
+    pset(6, x + 2, y + 1);
+    pset(6, x + 3, y + 1);
+    pset(6, x + 1, y + 2);
+    pset(6, x + 2, y + 2);
+    pset(6, x + 1, y + 3);
+    pset(5, x + 2, y + 3);
+    pset(5, x + 3, y + 3);
+    pset(5, x + 3, y + 2);
+  },
+  tree: (x, y) => {
+    // draw.grass(x, y);
+    pset(11, x + 1, y + 1);
+    pset(11, x, y + 2);
+    pset(11, x + 1, y + 2);
+    pset(11, x + 2, y + 2);
+    pset(5, x + 1, y + 3);
+  },
+  farmer: (x, y) => {
+    draw.grass(x, y);
+
+    let xx = 0;
+
+    pset(15, xx + x + 1, y);
+    pset(2, xx + x + 1, y + 1);
+    pset(1, xx + x + 1, y + 2);
+  },
+};
+
+callbacks.ontick = (delta: number) => {
+  // draw each tile
+  for (let y = 0; y < 45; y++) {
+    for (let x = 0; x < 80; x++) {
+      const fn = draw[tiles[y][x].type];
+      fn(x * 4, y * 4);
+    }
+  }
+
+  // draw mouse selection
+  if (mouse.drag) {
+    const x1 = mouse.drag.x;
+    const y1 = mouse.drag.y;
+    const x2 = mouse.x;
+    const y2 = mouse.y;
+
+    const x = x1 < x2 ? x1 : x2;
+    const y = y1 < y2 ? y1 : y2;
+    const w = x1 < x2 ? x2 - x1 : x1 - x2;
+    const h = y1 < y2 ? y2 - y1 : y1 - y2;
+
+    context.fillStyle = mouse.drag.b === 0 ? '#00f3' : '#f003';
+    context.fillRect(x, y, w + 1, h + 1);
+
+    context.strokeStyle = mouse.drag.b === 0 ? '#00f3' : '#f003';
+    context.strokeRect(x + .5, y + .5, w, h);
+  }
+
+  // draw mouse
+  context.fillStyle = '#0007';
+  context.fillRect(mouse.x - 2, mouse.y, 5, 1);
+  context.fillRect(mouse.x, mouse.y - 2, 1, 5);
+  context.fillStyle = '#fff';
+  context.fillRect(mouse.x, mouse.y, 1, 1);
+};
