92c6dba3cfe700facd9d010fca9bd6d13b458eb0
sdegutis <sbdegutis@gmail.com>
2025-02-13 16:13:53 -0600

    Print and draw.

--- site/ui.ts
+++ site/ui.ts
@@ -23,10 +23,16 @@ export class Box {
   ) { }
 
   draw() {
-    if (this.background) {
-      rectfill(0, 0, this.w, this.h, this.background);
-    }
+    this.drawBackground();
+    this.drawChildren();
+  }
 
+  drawBackground() {
+    if (!this.background) return;
+    rectfill(0, 0, this.w, this.h, this.background);
+  }
+
+  drawChildren() {
     for (let i = 0; i < this.children.length; i++) {
       const child = this.children[i];
       camera.x += child.x;
@@ -122,23 +128,23 @@ canvas.oncontextmenu = (e) => { e.preventDefault(); };
 
 
 
-
-
-
-
-
+let tick = (delta: number) => { };
 let last = +document.timeline.currentTime!;
+
 function update(t: number) {
   if (t - last >= 30) {
-    context.clearRect(0, 0, 320, 180);
-    const delta = t - last;
-    last = t;
-    // allthings.tick(delta);
+    tick(t - last);
     root.draw();
+    last = t;
   }
   requestAnimationFrame(update);
 }
 requestAnimationFrame(update);
+
+export function ontick(fn: (delta: number) => void) {
+  tick = fn;
+}
+
 
 
 
@@ -273,4 +279,87 @@ export class Button extends Box {
 
   }
 
+}
+
+
+
+
+
+
+
+
+
+
+const mapping = `abcdefghijklmnopqrstuvwxyz .,'!?1234567890-+/()":;%*=[]<>_`;
+const src = `
+ xxx     xx      xxx     xx      xxx     xxx     xxx     x x     xxx     xxx     x x     x       xxx     xxx     xxx     xxx  |
+ x x     xxx     x       x x     xx      xx      x       xxx      x       x      xx      x       xxx     x x     x x     x x  |
+ xxx     x x     x       x x     x       x       x x     x x      x       x      xx      x       x x     x x     x x     xx   |
+ x x     xxx     xxx     xx      xxx     x       xxx     x x     xxx     xx      x x     xxx     x x     x x     xxx     x    |
+                                                                                                                              |
+ xxx     xxx     xxx     xxx     x x     x x     x x     x x     x x     xxx                             xx       x      xxx  |
+ x x     x x     x        x      x x     x x     x x      x      x x      xx                              x       x      x x  |
+ xxx     xx       xx      x      x x     x x     xxx      x       x      x                        x                           |
+   x     x x     xxx      x      xxx      x      xxx     x x      x      xxx              x      xx               x        x  |
+                                                                                                                              |
+ xx      xx      xxx     x x     xxx     xxx     xxx     xxx     xxx      x               x        x       x      x      x x  |
+  x        x      xx     x x     xx      x         x     xxx     x x     x x     xxx     xxx      x       x        x     x x  |
+  x       x        x     xxx       x     xxx       x     x x      xx     x x              x       x       x        x          |
+ xxx     xxx     xxx       x     xx      xxx       x     xxx     xx       x                      x         x      x           |
+                                                                                                                              |
+ x        x       x      x x     xx      xx      xx       x      x                                                            |
+                          x              x        x      x        x                                                           |
+ x        x      xxx     x x     xx      x        x       x      x                                                            |
+         x        x                      xx      xx                      xxx                                                  |`;
+
+const chars: Record<string, boolean[][]> = {};
+
+for (let i = 0; i < mapping.length; i++) {
+  const c = mapping[i];
+
+  const grid: boolean[][] = [];
+  chars[c] = grid;
+
+  for (let y = 0; y < 4; y++) {
+    const row: boolean[] = [];
+    grid.push(row);
+
+    for (let x = 0; x < 4; x++) {
+      const px = (i % 16) * 8 + 1 + x;
+      const py = (Math.floor(i / 16) * 5) + y;
+      const index = 1 + (px + py * 16 * 8);
+
+      row.push(src[index] === ' ' ? false : true);
+    }
+  }
+}
+
+export function print(x: number, y: number, c: string, text: string) {
+  let posx = 0;
+  let posy = 0;
+
+  for (let i = 0; i < text.length; i++) {
+    const ch = text[i];
+
+    if (ch === '\n') {
+      posy++;
+      posx = 0;
+      continue;
+    }
+
+    const map = chars[ch];
+
+    for (let yy = 0; yy < 4; yy++) {
+      for (let xx = 0; xx < 4; xx++) {
+        const px = x + (posx * 4) + xx;
+        const py = y + (posy * 6) + yy;
+
+        if (map[yy][xx]) {
+          pset(px, py, c);
+        }
+      }
+    }
+
+    posx++;
+  }
 }
--- site/uitest.ts
+++ site/uitest.ts
@@ -1,6 +1,15 @@
-import { Box, Button, DragHandle, drawrect, mouse, pset, root } from "./ui.js";
+import { Box, Button, DragHandle, drawrect, mouse, ontick, print, pset, rectfill, root } from "./ui.js";
+
+const COLORS = [
+  '#000000', '#1D2B53', '#7E2553', '#008751',
+  '#AB5236', '#5F574F', '#C2C3C7', '#FFF1E8',
+  '#FF004D', '#FFA300', '#FFEC27', '#00E436',
+  '#29ADFF', '#83769C', '#FF77A8', '#FFCCAA',
+];
 
 
+
+root.background = '#000';
 
 const box1 = new Box(10, 10, 20, 40, '#ff03');
 root.children.push(box1);
@@ -11,7 +20,7 @@ box1.children.push(box2);
 const button = new Button(0, 30, 5, 5, '#f007');
 box1.children.push(button);
 
-const handle = new DragHandle(box1, 0, 0, 8, 8, '#0003');
+const handle = new DragHandle(box1, 0, 0, 8, 8, '#fff3');
 box1.children.push(handle);
 
 button.onClick = () => {
@@ -25,6 +34,13 @@ button.draw = () => {
 
   drawrect(0, 0, button.w, button.h, col);
   pset(0, 0, '#fff');
+
+};
+
+box2.draw = () => {
+  box2.drawChildren();
+  box2.drawBackground();
+  print(1, 1, '#f00', 'test');
 };
 
 
@@ -32,3 +48,9 @@ const cursor = new Box(0, 0, 320, 180);
 cursor.passthrough = true;
 cursor.draw = () => pset(mouse.x, mouse.y, '#00f');
 root.children.push(cursor);
+
+ontick(delta => {
+  // rectfill(0, 0, 320, 180, '#000');
+
+
+});
