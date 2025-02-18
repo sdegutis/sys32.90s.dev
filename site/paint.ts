c11ba6751e99ea5ab9fc0a2563ba373cb4f32aa2
sdegutis <sbdegutis@gmail.com>
2025-02-18 06:46:32 -0600

    Fixes.

--- site/paint.ts
+++ site/paint.ts
@@ -1,21 +1,5 @@
 import { Bitmap, Box, Font, MouseTracker, Screen, dragMove } from "./crt.js";
 
-// static props: Property[] = [
-//   { name: 'x', type: 'number' },
-//   { name: 'y', type: 'number' },
-//   { name: 'w', type: 'number' },
-//   { name: 'h', type: 'number' },
-//   { name: 'background', type: 'color' },
-// ];
-
-// export type Property = {
-//   name: string,
-//   type: 'number' | 'string' | 'color' | 'boolean',
-// };
-
-const screen = new Screen(document.querySelector('canvas')!);
-screen.autoscale();
-
 class BorderBox extends Box {
 
   border = 0xffffff33;
@@ -64,7 +48,7 @@ class SplitBoxDivider extends Box {
 
   draw(): void {
     if (this.hovered) {
-      screen.rectFill(0, 0, this.w, this.h, this.split.dividerColorHover);
+      this.screen.rectFill(0, 0, this.w, this.h, this.split.dividerColorHover);
     }
   }
 
@@ -72,7 +56,7 @@ class SplitBoxDivider extends Box {
     const cursor = this.split.dir === 'x' ?
       SplitBoxDivider.xresize :
       SplitBoxDivider.yresize;
-    cursor.bitmap.draw(screen, x - cursor.offset[0], y - cursor.offset[1]);
+    cursor.bitmap.draw(this.screen, x - cursor.offset[0], y - cursor.offset[1]);
   }
 
   onMouseDown(trackMouse: MouseTracker): void {
@@ -114,7 +98,7 @@ class SplitBox extends Box {
   get resizable() { return this.#resizer !== undefined; }
   set resizable(should: boolean) {
     if (should) {
-      this.#resizer = new SplitBoxDivider(screen, this);
+      this.#resizer = new SplitBoxDivider(this.screen, this);
       this.children = [this.a, this.b, this.#resizer];
     }
     else {
@@ -124,9 +108,6 @@ class SplitBox extends Box {
   }
 
   layout(): void {
-    // this.children = [this.a, this.b];
-    if (this.#resizer) this.children.push(this.#resizer);
-
     const dx = this.dir;
     const dw = dx === 'x' ? 'w' : 'h';
 
@@ -159,187 +140,203 @@ const vacuumLayout = function (this: Box) {
   this.children[0].w = this.w;
   this.children[0].h = this.h;
 };
+{
+  const screen = new Screen(document.querySelector('canvas')!);
+  screen.autoscale();
 
-screen.root.layout = vacuumLayout;
+  screen.root.layout = vacuumLayout;
 
-const split = new SplitBox(screen);
-// split.w = 320;
-// split.h = 180;
-split.pos = 10;
-split.min = 8;
-split.max = 18;
-split.dir = 'y';
+  const split = new SplitBox(screen);
+  // split.w = 320;
+  // split.h = 180;
+  split.pos = 10;
+  split.min = 8;
+  split.max = 18;
+  split.dir = 'y';
 
-const red = new BorderBox(screen); red.background = 0x330000ff; red.border = 0xffffff00;
-const green = new BorderBox(screen); green.background = 0x003300ff; green.border = 0xffffff00;
-const blue = new BorderBox(screen); blue.background = 0x000033ff; blue.border = 0xffffff00;
+  const red = new BorderBox(screen); red.background = 0x330000ff; red.border = 0xffffff00;
+  const green = new BorderBox(screen); green.background = 0x003300ff; green.border = 0xffffff00;
+  const blue = new BorderBox(screen); blue.background = 0x000033ff; blue.border = 0xffffff00;
 
-const split2 = new SplitBox(screen);
-split2.resizable = true;
-split2.pos = 30;
-split2.min = 28;
-split2.max = 38;
-split2.dir = 'x';
+  const split2 = new SplitBox(screen);
+  split2.resizable = true;
+  split2.pos = 30;
+  split2.min = 28;
+  split2.max = 38;
+  split2.dir = 'x';
 
-screen.root.children.push(split);
+  screen.root.children.push(split);
 
-split.a.layout = vacuumLayout;
-split.b.layout = vacuumLayout;
-split2.a.layout = vacuumLayout;
-split2.b.layout = vacuumLayout;
+  split.a.layout = vacuumLayout;
+  split.b.layout = vacuumLayout;
+  split2.a.layout = vacuumLayout;
+  split2.b.layout = vacuumLayout;
 
-split.a.children.push(blue);
-split.b.children.push(split2);
+  split.a.children.push(blue);
+  split.b.children.push(split2);
 
-split2.a.children.push(red);
-split2.b.children.push(green);
+  split2.a.children.push(red);
+  split2.b.children.push(green);
 
-split.resizable = true;
-
-
-
-
-
-
-
-class GridBox extends Box {
-
-  // static props: Property[] = [
-  //   ...super.props,
-  //   { name: 'id', type: 'string' },
-  // ];
-
-}
-
-
-// console.log(GridBox.props)
+  split.resizable = true;
 
 
 
 
 
 
-class Label extends Box {
 
-  #text = '';
-  font = Font.crt2025;
-  padding = 1;
-  passthrough = true;
+  // // static props: Property[] = [
+  // //   { name: 'x', type: 'number' },
+  // //   { name: 'y', type: 'number' },
+  // //   { name: 'w', type: 'number' },
+  // //   { name: 'h', type: 'number' },
+  // //   { name: 'background', type: 'color' },
+  // // ];
 
-  constructor(screen: Screen, text: string) {
-    super(screen);
-    this.text = text;
+  // // export type Property = {
+  // //   name: string,
+  // //   type: 'number' | 'string' | 'color' | 'boolean',
+  // // };
+
+  //   class GridBox extends Box {
+
+  //     // static props: Property[] = [
+  //     //   ...super.props,
+  //     //   { name: 'id', type: 'string' },
+  //     // ];
+
+  //   }
+
+  //   // console.log(GridBox.props)
+
+
+
+
+
+
+  class Label extends Box {
+
+    #text = '';
+    font = Font.crt2025;
+    padding = 1;
+    passthrough = true;
+
+    constructor(screen: Screen, text: string) {
+      super(screen);
+      this.text = text;
+    }
+
+    get text() { return this.#text; }
+    set text(s: string) {
+      this.#text = s;
+      const size = this.font.calcSize(s);
+      this.w = size.w + this.padding * 2;
+      this.h = size.h + this.padding * 2;
+    }
+
+    draw() {
+      this.screen.print(this.padding, this.padding, 0xffffffff, this.text);
+    }
+
   }
 
-  get text() { return this.#text; }
-  set text(s: string) {
-    this.#text = s;
-    const size = this.font.calcSize(s);
-    this.w = size.w + this.padding * 2;
-    this.h = size.h + this.padding * 2;
-  }
 
-  draw() {
-    this.screen.print(this.padding, this.padding, 0xffffffff, this.text);
-  }
+  class Button extends BorderBox {
 
-}
+    padding = 5;
+    pressColor = 0x00000033;
+    hoverColor = 0x00000000;
 
+    onClick?(): void;
 
-class Button extends BorderBox {
+    pressed = false;
 
-  padding = 5;
-  pressColor = 0x00000033;
-  hoverColor = 0x00000000;
+    children: Box[] = [new Label(screen, 'button')];
 
-  onClick?(): void;
+    get child() { return this.children[0]; }
+    set child(child: Box) {
+      this.children = [child];
+      child.x = this.padding;
+      child.y = this.padding;
+      this.w = child.w + this.padding * 2;
+      this.h = child.h + this.padding * 2;
+    }
 
-  pressed = false;
-
-  children: Box[] = [new Label(screen, 'button')];
-
-  get child() { return this.children[0]; }
-  set child(child: Box) {
-    this.children = [child];
-    child.x = this.padding;
-    child.y = this.padding;
-    this.w = child.w + this.padding * 2;
-    this.h = child.h + this.padding * 2;
-  }
-
-  onMouseDown(trackMouse: MouseTracker): void {
-    this.pressed = true;
-    trackMouse({
-      move: () => {
-        if (!this.hovered) {
+    onMouseDown(trackMouse: MouseTracker): void {
+      this.pressed = true;
+      trackMouse({
+        move: () => {
+          if (!this.hovered) {
+            this.pressed = false;
+          }
+        },
+        up: () => {
+          if (this.pressed) {
+            this.onClick?.();
+          }
           this.pressed = false;
-        }
-      },
-      up: () => {
-        if (this.pressed) {
-          this.onClick?.();
-        }
-        this.pressed = false;
-      },
-    });
+        },
+      });
+    }
+
+    draw(): void {
+      super.draw();
+      if (this.pressed) {
+        this.screen.rectFill(0, 0, this.w, this.h, this.pressColor);
+      }
+      else if (this.hovered) {
+        this.screen.rectFill(0, 0, this.w, this.h, this.hoverColor);
+      }
+    }
+
   }
 
-  draw(): void {
-    super.draw();
-    if (this.pressed) {
-      this.screen.rectFill(0, 0, this.w, this.h, this.pressColor);
-    }
-    else if (this.hovered) {
-      this.screen.rectFill(0, 0, this.w, this.h, this.hoverColor);
-    }
-  }
+  const button = new Button(screen);
+  button.x = 30;
+  button.y = 30;
+  button.background = 0x00000033;
+  button.border = 0xff000033;
+  green.children.push(button);
 
+  const label = new Label(screen, 'yes \\n no');
+
+  button.child = label;
+  button.onClick = () => console.log('clicked')
+
+  // button.children.push(label);
+
+
+  screen.layoutTree();
+
+  green.background = 0x222222ff;
+
+  const button2 = new Button(screen);
+  button2.padding = 2;
+  button2.x = 90;
+  button2.y = 30;
+  button2.background = 0x00000033;
+  button2.border = 0x999999ff;
+  green.children.push(button2);
+
+
+  // button2.onMouseDown = (t) => {
+  //   t({
+  //     move: () => {
+
+  //     },
+  //     up: () => {
+  //       screen.pset(screen.mouse.x, screen.mouse.y, 0xffffff99)
+  //       console.log(screen.#hovered.mouse.x, screen.#hovered.mouse.y, screen.#hovered)
+
+  //     },
+  //   })
+  // };
+
+  const b = new Box(screen);
+  b.background = 0x990000ff;
+  b.passthrough = true;
+  b.w = 3;
+  b.h = 3;
+  button2.child = b;
 }
-
-const button = new Button(screen);
-button.x = 30;
-button.y = 30;
-button.background = 0x00000033;
-button.border = 0xff000033;
-green.children.push(button);
-
-const label = new Label(screen, 'yes \\n no');
-
-button.child = label;
-button.onClick = () => console.log('clicked')
-
-// button.children.push(label);
-
-
-screen.layoutTree();
-
-green.background = 0x222222ff;
-
-const button2 = new Button(screen);
-button2.padding = 2;
-button2.x = 90;
-button2.y = 30;
-button2.background = 0x00000033;
-button2.border = 0x999999ff;
-green.children.push(button2);
-
-
-// button2.onMouseDown = (t) => {
-//   t({
-//     move: () => {
-
-//     },
-//     up: () => {
-//       screen.pset(screen.mouse.x, screen.mouse.y, 0xffffff99)
-//       console.log(screen.#hovered.mouse.x, screen.#hovered.mouse.y, screen.#hovered)
-
-//     },
-//   })
-// };
-
-const b = new Box(screen);
-b.background = 0x990000ff;
-b.passthrough = true;
-b.w = 3;
-b.h = 3;
-button2.child = b;
