09fe31ab9952602efdf6d7f77ac34ece0a4a22ea
sdegutis <sbdegutis@gmail.com>
2025-02-18 13:33:17 -0600

    Load UI test dynamically!

--- site/paint.ts
+++ site/paint.ts
@@ -1,4 +1,4 @@
-import { BorderBox, Box } from "./crt/box.js";
+import { Box } from "./crt/box.js";
 import { Button } from "./crt/button.js";
 import { Label } from "./crt/label.js";
 import { centerLayout, makeFlowLayout, vacuumLayout } from "./crt/layouts.js";
@@ -17,28 +17,37 @@ screen.root.layout = vacuumLayout;
 
 
 
+const split2 = make(screen, SplitBox, { pos: 30, min: 28, max: 8, dir: 'x', resizable: true },
+  make(screen, Box, { background: 0x330000ff, layout: centerLayout },
+    make(screen, Button, {
+      background: 0x00000033, border: 0xff000033, onClick: () => {
+        import('./uitest.js').then((mod) => {
+          split2.children[1] = mod.default(screen);
+          screen.layoutTree(split2);
+        });
+      }
+    },
+      make(screen, Label, { text: 'hello\nworld' })
+    )
+  ),
+  make(screen, Box, { background: 0x003300ff, layout: makeFlowLayout(3, 3) },
+    ...Array(20).fill(0).map((_, i) => make(screen, Button, { padding: 2, background: 0x00000033, border: 0x999999ff, onClick: () => console.log('color', i) },
+      randomColorSquare(Math.floor(i / 3) + 6)
+    )),
+    make(screen, TextField, {
+      background: 0x000000aa,
+      border: 0xffffff77,
+      color: 0xffffffff,
+      padding: 3,
+      text: 'tesin',
+    })
+  )
+);
+
 screen.root.children = [
   make(screen, SplitBox, { pos: 10, min: 8, max: 18, dir: 'y', resizable: true },
     make(screen, Box, { background: 0x000033ff }),
-    make(screen, SplitBox, { pos: 30, min: 28, max: 8, dir: 'x', resizable: true },
-      make(screen, Box, { background: 0x330000ff, layout: centerLayout },
-        make(screen, Button, { background: 0x00000033, border: 0xff000033, onClick: () => console.log('clicked') },
-          make(screen, Label, { text: 'hello\nworld' })
-        )
-      ),
-      make(screen, Box, { background: 0x003300ff, layout: makeFlowLayout(3, 3) },
-        ...Array(20).fill(0).map((_, i) => make(screen, Button, { padding: 2, background: 0x00000033, border: 0x999999ff, onClick: () => console.log('color', i) },
-          randomColorSquare(Math.floor(i / 3) + 6)
-        )),
-        make(screen, TextField, {
-          background: 0x000000aa,
-          border: 0xffffff77,
-          color: 0xffffffff,
-          padding: 3,
-          text: 'tesin',
-        })
-      ),
-    ),
+    split2,
   )
 ];
 
