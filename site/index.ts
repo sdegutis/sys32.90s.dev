e69df15f7a7b458f5961be128dbb2898538c9afc
sdegutis <sbdegutis@gmail.com>
2025-02-21 14:12:59 -0600

    Clock.

--- site/index.ts
+++ site/index.ts
@@ -1,102 +1,102 @@
 import demo from "./apps/demo.js";
 import mapmaker from "./apps/mapmaker.js";
+import { Group } from "./sys32/containers/group.js";
+import { Paned } from "./sys32/containers/paned.js";
 import { Panel } from "./sys32/containers/panel.js";
+import { Spaced } from "./sys32/containers/spaced.js";
 import { Split } from "./sys32/containers/split.js";
+import { Button } from "./sys32/controls/button.js";
+import { Label } from "./sys32/controls/label.js";
 import { System } from "./sys32/core/system.js";
 import { View } from "./sys32/core/view.js";
 import { centerLayout, makeVacuumLayout } from "./sys32/util/layouts.js";
-
-
-
-
-
-// let i = 0;
-
-// export class Workspace {
-
-//   sys: System;
-//   desktop;
-//   taskbar;
-
-//   constructor(panel: Panel) {
-//     this.sys = sys;
-
-//     const b = makeBuilder(panel);
-//     this.desktop = b(View, { background: 0x333333ff });
-//     this.taskbar = b(Spaced, { background: 0x000000ff },
-//       b(Group, { background: 0x222222ff },
-//         b(Button, {
-//           padding: 2, onClick() {
-//             // i++
-//             i = (i + 1) % 2;
-//             sys.resize(320 * (i + 1), 180 * (i + 1));
-//           }
-//         }, b(Label, { text: 'one' }))
-//       ),
-//       b(Group, { background: 0x222222ff },
-//       )
-//     );
-
-//     sys.root.children = [
-//       b(Paned, { vacuum: 'b', dir: 'y' },
-//         this.desktop,
-//         this.taskbar,
-//       )
-//     ];
-
-//     sys.layoutTree();
-//   }
-
-// }
-
-
-
-
-
-
-
-
 
 
 const canvas = document.querySelector('canvas')!;
 const sys = new System(canvas);
 sys.resize(320 * 2, 180 * 2);
 sys.crt.autoscale();
-// sys.root.view.layout = makeVacuumLayout(10);
+sys.root.layout = makeVacuumLayout();
 sys.root.background = 0x330000ff;
 
 
 const $ = sys.make.bind(sys);
 
 
-sys.root.addChild($(Panel, {
-  title: 'demo',
-  x: 70, y: 90, w: 400, h: 300,
-  content: $(View, { layout: centerLayout, background: 0x22222299 },
-    demo(sys)
+
+class ClockView extends Group {
+
+  #label = $(Label);
+
+  override init(): void {
+    this.children = [this.#label];
+    this.#updateTime();
+    setInterval((() => {
+      this.#updateTime();
+      sys.layoutTree();
+    }), 1000);
+  }
+
+  #updateTime() {
+    this.#label.text = new Date().toLocaleTimeString('en-us');
+  }
+
+}
+
+
+// let i = 0;
+
+const desktop = $(View, { background: 0x333333ff });
+const taskbar = $(Spaced, { background: 0x000000ff },
+  $(Group, { background: 0x222222ff },
+    $(Button, {
+      padding: 2, onClick() {
+        // i = (i + 1) % 2;
+        // sys.resize(320 * (i + 1), 180 * (i + 1));
+      }
+    }, $(Label, { text: 'one' }))
+  ),
+  $(Group, { background: 0x222222ff },
+    $(ClockView)
   )
-}));
+);
 
-
-sys.root.addChild($(Panel, {
-  title: 'mapmaker',
-  x: 30, y: 10, w: 400, h: 300,
-  content: mapmaker(sys)
-}));
-
-
-sys.root.addChild($(Panel, {
-  title: 'both',
-  x: 90, y: 110, w: 400, h: 300,
-  content: $(View, { layout: makeVacuumLayout(), background: 0x000033ff },
-    $(Split, { pos: 200, dir: 'x', resizable: true },
-      $(View, { layout: centerLayout, background: 0x003333ff },
-        demo(sys)
-      ),
-      mapmaker(sys)
-    )
+sys.root.children = [
+  $(Paned, { vacuum: 'b', dir: 'y' },
+    desktop,
+    taskbar,
   )
-}));
+];
+
+
+// sys.root.addChild($(Panel, {
+//   title: 'demo',
+//   x: 70, y: 90, w: 400, h: 300,
+//   content: $(View, { layout: centerLayout, background: 0x22222299 },
+//     demo(sys)
+//   )
+// }));
+
+
+// sys.root.addChild($(Panel, {
+//   title: 'mapmaker',
+//   x: 30, y: 10, w: 400, h: 300,
+//   content: mapmaker(sys)
+// }));
+
+
+// sys.root.addChild($(Panel, {
+//   title: 'both',
+//   x: 90, y: 110, w: 400, h: 300,
+//   content: $(View, { layout: makeVacuumLayout(), background: 0x000033ff },
+//     $(Split, { pos: 200, dir: 'x', resizable: true },
+//       $(View, { layout: centerLayout, background: 0x003333ff },
+//         demo(sys)
+//       ),
+//       mapmaker(sys)
+//     )
+//   )
+// }));
 
 
 sys.layoutTree();
