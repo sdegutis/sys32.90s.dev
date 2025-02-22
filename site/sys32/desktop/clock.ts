d08ebc0b1ccb4ff8b5de82c765a225882b5969a1
sdegutis <sbdegutis@gmail.com>
2025-02-21 21:30:11 -0600

    lol

--- site/sys32/desktop/clock.ts
+++ site/sys32/desktop/clock.ts
@@ -24,4 +24,20 @@ export class Clock extends Label {
     this.text = new Date().toLocaleTimeString('en-us');
   }
 
+  // override draw(): void {
+  //   super.draw();
+
+  //   const old = { ...      this.sys.crt.clip };
+  //   this.sys.crt.clip.cx = 0;
+  //   this.sys.crt.clip.cy = 0;
+  //   this.sys.crt.clip.x1 = 0;
+  //   this.sys.crt.clip.y1 = 0;
+  //   this.sys.crt.clip.x2 = 320;
+  //   this.sys.crt.clip.y2 = 180;
+
+  //   this.sys.crt.rectFill(10, 2, 200, 100, 0x0000ff88)
+
+  //   Object.assign(this.sys.crt.clip, old);
+  // }
+
 }
