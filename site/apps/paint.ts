e7edf20e2cbdb24dea51f655ce61803c35560573
sdegutis <sbdegutis@gmail.com>
2025-02-23 13:48:48 -0600

    Basic paint idea.

--- site/apps/paint.ts
+++ site/apps/paint.ts
@@ -1,5 +1,6 @@
 import { GroupX, GroupY } from "../sys32/containers/group.js";
 import { PanedYB } from "../sys32/containers/paned.js";
+import { Scroll } from "../sys32/containers/scroll.js";
 import { Button } from "../sys32/controls/button.js";
 import { Label } from "../sys32/controls/label.js";
 import { TextField } from "../sys32/controls/textfield.js";
@@ -7,21 +8,80 @@ import { System } from "../sys32/core/system.js";
 import { View } from "../sys32/core/view.js";
 import { Panel } from "../sys32/desktop/panel.js";
 
+class PaintView extends View {
+
+  width = 10;
+  height = 10;
+
+  override background = 0xffffff33;
+
+  #grid: number[] = [];
+
+  override init(): void {
+    this.w = this.width * 4;
+    this.h = this.height * 4;
+  }
+
+  override draw(): void {
+    for (let x = 0; x < this.width; x++) {
+      this.sys.crt.rectFill(x * 4, 0, 1, this.w, 0x00000033);
+    }
+    for (let y = 0; y < this.height; y++) {
+      this.sys.crt.rectFill(0, y * 4, this.w, 1, 0x00000033);
+    }
+
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        const i = y * this.width + x;
+        const c = this.#grid[i];
+        if (c !== undefined) {
+          const px = x * 4;
+          const py = y * 4;
+          this.sys.crt.rectFill(px, py, 4, 4, c);
+        }
+      }
+    }
+
+    const px = Math.floor(this.mouse.x / 4) * 4;
+    const py = Math.floor(this.mouse.y / 4) * 4;
+    this.sys.crt.rectFill(px, py, 4, 4, 0x0000ff77);
+  }
+
+  override onMouseDown(): void {
+    this.sys.trackMouse({
+      move: () => {
+        const x = Math.floor(this.mouse.x / 4);
+        const y = Math.floor(this.mouse.y / 4);
+        const i = y * this.width + x;
+        this.#grid[i] = 0xffffffff;
+      }
+    })
+
+  }
+
+}
+
 export default function paint(sys: System) {
-  // const widthLabel = sys.make(TextField, { length: 3, background: 0x000000ff, padding: 2 });
-  // const heightLabel = sys.make(TextField, { length: 3, background: 0x000000ff, padding: 2 });
+  const paintView = sys.make(PaintView);
 
-  // widthLabel.onChange = () => {
+  const panel = sys.make(Panel, { title: 'paint' },
+    sys.make(Scroll, {
+      background: 0x222222ff,
+      draw() {
+        let off = 0;
+        const w = 4;
+        const h = 3;
+        for (let y = 0; y < this.h!; y++) {
+          for (let x = 0; x < this.w!; x += w) {
+            sys.crt.pset(off + x, y, 0x272727ff);
+          }
+          if (y % h === (h - 1)) off = (off + 1) % w;
+        }
+      }
+    },
+      paintView
+    )
 
-  //   console.log(widthLabel.text)
-  // }
-
-
-  const panel = sys.make(Panel, {
-    title: 'paint',
-    background: 0x003300ff
-  },
-    sys.make(View)
     // sys.make(PanedYB, {},
     //   sys.make(View, { background: 0x111111ff }),
     //   sys.make(GroupY, { background: 0x0000ff33 },
