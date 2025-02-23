0f89706903342d56d5683bbb2dd7fc2e74a659dc
sdegutis <sbdegutis@gmail.com>
2025-02-23 09:22:00 -0600

    Tests...

--- site/apps/paint.ts
+++ site/apps/paint.ts
@@ -2,24 +2,49 @@ import { GroupX, GroupY } from "../sys32/containers/group.js";
 import { PanedYB } from "../sys32/containers/paned.js";
 import { Button } from "../sys32/controls/button.js";
 import { Label } from "../sys32/controls/label.js";
+import { TextField } from "../sys32/controls/textfield.js";
 import { View } from "../sys32/core/view.js";
 import { Panel } from "../sys32/desktop/panel.js";
 import { Workspace } from "../sys32/desktop/workspace.js";
 
 export default function paint(ws: Workspace) {
+  const sys = ws.sys;
 
-  const panel = ws.sys.make(Panel, {
+  const widthLabel = sys.make(TextField, { length: 3, background: 0x000000ff, padding: 2 });
+  const heightLabel = sys.make(TextField, { length: 3, background: 0x000000ff, padding: 2 });
+
+  widthLabel.onChange = () => {
+
+    console.log(widthLabel.text)
+  }
+
+
+  const panel = sys.make(Panel, {
     title: 'paint',
-    content: ws.sys.make(PanedYB, {},
-      ws.sys.make(View, { background: 0x111111ff }),
-      ws.sys.make(GroupY, {},
-        ws.sys.make(View, { h: 3, background: 0x00ff0033 }),
-        ws.sys.make(GroupX, {},
-          ws.sys.make(Button, {}, ws.sys.make(Label, { text: 'hey' }))
+    content: sys.make(PanedYB, {},
+      sys.make(View, { background: 0x111111ff }),
+      sys.make(GroupY, { background: 0x0000ff33 },
+        sys.make(View, { h: 3, background: 0x00ff00ff }),
+        sys.make(GroupX, {},
+
+          sys.make(Label, { text: 'w:' }),
+          widthLabel,
+
+          sys.make(Label, { text: 'h:' }),
+          heightLabel,
+
+          sys.make(Button, {
+            padding: 2,
+            onClick: () => {
+              console.log('hey')
+            }
+          }, sys.make(Label, { text: 'resize' }))
         ),
       )
     ),
   })
-  ws.addPanel(panel);
 
+  sys.layoutTree(panel)
+
+  ws.addPanel(panel);
 }
--- site/index.ts
+++ site/index.ts
@@ -1,8 +1,13 @@
 import demo from "./apps/demo.js";
 import mapmaker from "./apps/mapmaker.js";
 import paint from "./apps/paint.js";
+import { Scroll } from "./sys32/containers/scroll.js";
+import { Label } from "./sys32/controls/label.js";
 import { System } from "./sys32/core/system.js";
+import { View } from "./sys32/core/view.js";
+import { Panel } from "./sys32/desktop/panel.js";
 import { Workspace } from "./sys32/desktop/workspace.js";
+import { makeVacuumLayout } from "./sys32/util/layouts.js";
 
 const canvas = document.querySelector('canvas')!;
 const sys = new System(canvas);
@@ -17,27 +22,22 @@ ws.addProgram('paint', paint);
 
 paint(ws);
 
-sys.layoutTree()
-
-// const panel = ws.newPanel({
-//   title: 'mapmaker',
-//   content: mapmaker(sys),
-// });
 
 
-// let text = '';
-// for (let i = 0; i < 23; i++) {
-//   text += String.fromCharCode(97 + i).repeat(i + 1) + '\n';
-// }
+let text = '';
+for (let i = 0; i < 23; i++) {
+  text += String.fromCharCode(97 + i).repeat(i + 1) + '\n';
+}
 
 
-// const panel = sys.make(Panel, {
-//   title: 'both',
-//   x: 40, y: 40, w: 70, h: 50,
-//   content: sys.make(View, { layout: makeVacuumLayout(), background: 0x44444433 },
-//     sys.make(Scroll, { amount: 6, background: 0x0000ff11 },
-//       sys.make(Label, { text, background: 0x00ff0011 })
-//     )
-//   )
-// });
-// sys.root.addChild(panel);
+const panel = sys.make(Panel, {
+  title: 'both',
+  x: 40, y: 40, w: 70, h: 50,
+  content: sys.make(View, { layout: makeVacuumLayout(), background: 0x44444433 },
+    sys.make(Scroll, { amount: 6, background: 0x0000ff11 },
+      sys.make(Label, { text, background: 0x00ff0011 })
+    )
+  )
+});
+sys.root.addChild(panel);
+sys.layoutTree(panel)
