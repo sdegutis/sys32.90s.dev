89d56eac016aa4dedd2999f45fb2d5ea0cf415a3
sdegutis <sbdegutis@gmail.com>
2025-02-23 17:44:05 -0600

    Rearrange.

--- site/apps/paint.ts
+++ site/apps/paint.ts
@@ -12,124 +12,6 @@ import { Panel } from "../sys32/desktop/panel.js";
 import { makeFlowLayout } from "../sys32/util/layouts.js";
 import { dragMove, dragResize } from "../sys32/util/selections.js";
 
-class ColorButton extends RadioButton {
-
-  color = 0x00000000;
-
-  override draw(): void {
-    this.sys.crt.rectFill(this.padding, this.padding, this.size, this.size, this.color);
-
-    if (this.checked) {
-      this.sys.crt.rectLine(0, 0, this.w, this.h, this.checkColor);
-    }
-    else if (this.hovered) {
-      this.sys.crt.rectLine(0, 0, this.w, this.h, 0xffffff77);
-    }
-  }
-
-}
-
-const COLORS = [
-  0x000000ff, 0x1D2B53ff, 0x7E2553ff, 0x008751ff,
-  0xAB5236ff, 0x5F574Fff, 0xC2C3C7ff, 0xFFF1E8ff,
-  0xFF004Dff, 0xFFA300ff, 0xFFEC27ff, 0x00E436ff,
-  0x29ADFFff, 0x83769Cff, 0xFF77A8ff, 0xFFCCAAff,
-];
-
-class PaintView extends View {
-
-  width = 10;
-  height = 10;
-
-  zoom = 4;
-
-  color = 0xffffffff;
-
-  tool: 'pencil' | 'eraser' = 'pencil';
-
-  override background = 0xffffff33;
-  override cursor: Cursor = { bitmap: new Bitmap([], 0, []), offset: [0, 0] };
-
-  #grid: number[] = [];
-
-  override adjust(): void {
-    this.w = this.width * this.zoom;
-    this.h = this.height * this.zoom;
-  }
-
-  override draw(): void {
-    for (let x = 0; x < this.width; x++) {
-      this.sys.crt.rectFill(x * this.zoom, 0, 1, this.h, 0x00000033);
-    }
-    for (let y = 0; y < this.height; y++) {
-      this.sys.crt.rectFill(0, y * this.zoom, this.w, 1, 0x00000033);
-    }
-
-    for (let y = 0; y < this.height; y++) {
-      for (let x = 0; x < this.width; x++) {
-        const i = y * this.width + x;
-        const c = this.#grid[i];
-        if (c !== undefined) {
-          const px = x * this.zoom;
-          const py = y * this.zoom;
-          this.sys.crt.rectFill(px, py, this.zoom, this.zoom, c);
-        }
-      }
-    }
-
-    if (this.#hovered) {
-      const px = Math.floor(this.mouse.x / this.zoom) * this.zoom;
-      const py = Math.floor(this.mouse.y / this.zoom) * this.zoom;
-      this.sys.crt.rectFill(px, py, this.zoom, this.zoom, 0x0000ff77);
-    }
-  }
-
-  override onMouseDown(): void {
-    if (this.tool === 'pencil' || this.tool === 'eraser') {
-      this.sys.trackMouse({
-        move: () => {
-          const x = Math.floor(this.mouse.x / this.zoom);
-          const y = Math.floor(this.mouse.y / this.zoom);
-          const i = y * this.width + x;
-          this.#grid[i] = this.tool === 'pencil' ? this.color : 0x00000000;
-        }
-      })
-    }
-  }
-
-  resize(width: number, height: number) {
-    const oldgrid = [...this.#grid];
-    const oldwidth = this.width;
-    const oldheight = this.height;
-
-    this.width = width;
-    this.height = height;
-    this.#grid.length = 0;
-
-    for (let y = 0; y < Math.min(height, oldheight); y++) {
-      for (let x = 0; x < Math.min(width, oldwidth); x++) {
-        const c = oldgrid[y * oldwidth + x];
-        if (c !== undefined) this.#grid[y * width + x] = c;
-      }
-    }
-
-    this.sys.layoutTree(this.parent);
-  }
-
-  #hovered = false;
-
-  override onMouseEnter(): void {
-    super.onMouseEnter?.();
-    this.#hovered = true;
-  }
-
-  override onMouseExit(): void {
-    super.onMouseExit?.();
-    this.#hovered = false;
-  }
-
-}
-
 export default function paint(sys: System) {
   const paintView = sys.make(PaintView, {});
 
@@ -279,6 +161,104 @@ export default function paint(sys: System) {
   sys.focus(panel);
 }
 
+
+
+
+
+class PaintView extends View {
+
+  width = 10;
+  height = 10;
+
+  zoom = 4;
+
+  color = 0xffffffff;
+
+  tool: 'pencil' | 'eraser' = 'pencil';
+
+  override background = 0xffffff33;
+  override cursor: Cursor = { bitmap: new Bitmap([], 0, []), offset: [0, 0] };
+
+  #grid: number[] = [];
+
+  override adjust(): void {
+    this.w = this.width * this.zoom;
+    this.h = this.height * this.zoom;
+  }
+
+  override draw(): void {
+    for (let x = 0; x < this.width; x++) {
+      this.sys.crt.rectFill(x * this.zoom, 0, 1, this.h, 0x00000033);
+    }
+    for (let y = 0; y < this.height; y++) {
+      this.sys.crt.rectFill(0, y * this.zoom, this.w, 1, 0x00000033);
+    }
+
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        const i = y * this.width + x;
+        const c = this.#grid[i];
+        if (c !== undefined) {
+          const px = x * this.zoom;
+          const py = y * this.zoom;
+          this.sys.crt.rectFill(px, py, this.zoom, this.zoom, c);
+        }
+      }
+    }
+
+    if (this.#hovered) {
+      const px = Math.floor(this.mouse.x / this.zoom) * this.zoom;
+      const py = Math.floor(this.mouse.y / this.zoom) * this.zoom;
+      this.sys.crt.rectFill(px, py, this.zoom, this.zoom, 0x0000ff77);
+    }
+  }
+
+  override onMouseDown(): void {
+    if (this.tool === 'pencil' || this.tool === 'eraser') {
+      this.sys.trackMouse({
+        move: () => {
+          const x = Math.floor(this.mouse.x / this.zoom);
+          const y = Math.floor(this.mouse.y / this.zoom);
+          const i = y * this.width + x;
+          this.#grid[i] = this.tool === 'pencil' ? this.color : 0x00000000;
+        }
+      })
+    }
+  }
+
+  resize(width: number, height: number) {
+    const oldgrid = [...this.#grid];
+    const oldwidth = this.width;
+    const oldheight = this.height;
+
+    this.width = width;
+    this.height = height;
+    this.#grid.length = 0;
+
+    for (let y = 0; y < Math.min(height, oldheight); y++) {
+      for (let x = 0; x < Math.min(width, oldwidth); x++) {
+        const c = oldgrid[y * oldwidth + x];
+        if (c !== undefined) this.#grid[y * width + x] = c;
+      }
+    }
+
+    this.sys.layoutTree(this.parent);
+  }
+
+  #hovered = false;
+
+  override onMouseEnter(): void {
+    super.onMouseEnter?.();
+    this.#hovered = true;
+  }
+
+  override onMouseExit(): void {
+    super.onMouseExit?.();
+    this.#hovered = false;
+  }
+
+}
+
 class Slider extends View {
 
   onChange?(): void;
@@ -314,14 +294,34 @@ class Slider extends View {
   override draw(): void {
     const y1 = Math.floor(this.h / 2);
     this.sys.crt.rectFill(0, y1, this.w, 1, 0xffffff33);
-
     const p = (this.val - this.min) / (this.max - this.min);
-
-    // console.log(this.val, (this.max - this.min), this.val / (this.max - this.min))
-
     const x = Math.floor(p * (this.w - this.knobSize));
     const y = Math.round(this.h / 2 - this.knobSize / 2);
     this.sys.crt.rectFill(x, y, 2, 2, 0xffffffff);
   }
 
 }
+
+class ColorButton extends RadioButton {
+
+  color = 0x00000000;
+
+  override draw(): void {
+    this.sys.crt.rectFill(this.padding, this.padding, this.size, this.size, this.color);
+
+    if (this.checked) {
+      this.sys.crt.rectLine(0, 0, this.w, this.h, this.checkColor);
+    }
+    else if (this.hovered) {
+      this.sys.crt.rectLine(0, 0, this.w, this.h, 0xffffff77);
+    }
+  }
+
+}
+
+const COLORS = [
+  0x000000ff, 0x1D2B53ff, 0x7E2553ff, 0x008751ff,
+  0xAB5236ff, 0x5F574Fff, 0xC2C3C7ff, 0xFFF1E8ff,
+  0xFF004Dff, 0xFFA300ff, 0xFFEC27ff, 0x00E436ff,
+  0x29ADFFff, 0x83769Cff, 0xFF77A8ff, 0xFFCCAAff,
+];
