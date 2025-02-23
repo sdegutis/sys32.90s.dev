77a9c5de2a42b71626dbd9ed8d3e98d55b286736
sdegutis <sbdegutis@gmail.com>
2025-02-22 21:28:06 -0600

    Cleanup.

--- site/index.ts
+++ site/index.ts
@@ -1,12 +1,8 @@
-import { demo } from "./apps/demo.js";
-import { mapmaker } from "./apps/mapmaker.js";
+import demo from "./apps/demo.js";
+import mapmaker from "./apps/mapmaker.js";
 import paint from "./apps/paint.js";
-import { Split } from "./sys32/containers/split.js";
 import { System } from "./sys32/core/system.js";
-import { View } from "./sys32/core/view.js";
-import { Panel } from "./sys32/desktop/panel.js";
 import { Workspace } from "./sys32/desktop/workspace.js";
-import { centerLayout, makeVacuumLayout } from "./sys32/util/layouts.js";
 
 const canvas = document.querySelector('canvas')!;
 const sys = new System(canvas);
@@ -15,30 +11,13 @@ sys.crt.autoscale();
 
 const ws = new Workspace(sys);
 
-const appPaths = await fetch('./apps.json').then<string[]>(res => res.json());
-appPaths.forEach(p => {
-  import(p).then(mod => {
-    const start = mod.default;
-    ws.addProgram(p.split('/').at(-1)!, start);
-  });
-})
-
-ws.addProgram('both', (ws) => {
-  const panel = sys.make(Panel, {
-    title: 'both',
-    content: sys.make(View, { layout: makeVacuumLayout(), background: 0x000033ff },
-      sys.make(Split, { pos: 200, min: 20, max: 20, dir: 'x', resizable: true },
-        sys.make(View, { layout: centerLayout, background: 0x003333ff },
-          demo(sys)
-        ),
-        mapmaker(sys)
-      )
-    )
-  })
-  ws.addPanel(panel);
-});
+ws.addProgram('demo', demo);
+ws.addProgram('mapmaker', mapmaker);
+ws.addProgram('paint', paint);
 
 paint(ws);
+
+sys.layoutTree()
 
 // const panel = ws.newPanel({
 //   title: 'mapmaker',
