e365d0d297f8ce589bac0bb8bfdf0c20263676d3
sdegutis <sbdegutis@gmail.com>
2025-02-27 13:07:09 -0600

    Cleanup.

--- site/main.ts
+++ site/main.ts
@@ -3,21 +3,11 @@ import filer from "./apps/filer.js";
 import mapmaker from "./apps/mapmaker.js";
 import painter from "./apps/painter.js";
 import writer from "./apps/writer.js";
-import { Scroll } from "./sys32/containers/scroll.js";
-import { TextArea } from "./sys32/containers/textarea.js";
 import { System } from "./sys32/core/system.js";
-import { View } from "./sys32/core/view.js";
-import { Panel } from "./sys32/desktop/panel.js";
 import { Workspace } from "./sys32/desktop/workspace.js";
-import { makeVacuumLayout } from "./sys32/util/layouts.js";
 
-
-const canvas = document.querySelector('canvas')!;
-const sys = new System(canvas);
-// sys.resize(320 * 2, 180 * 2);
+const sys = new System(document.querySelector('canvas')!);
 sys.crt.autoscale();
-// sys.root.background = 0x003300ff;
-// sys.root.layout = makeVacuumLayout()
 
 writer(sys);
 // mapmaker(sys);
@@ -25,7 +15,7 @@ writer(sys);
 // demo(sys)
 // painter(sys);
 // texttest(sys);
-sys.layoutTree()
+// sys.layoutTree()
 
 const ws = new Workspace(sys);
 ws.addProgram('demo', demo);
