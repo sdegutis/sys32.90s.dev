0597cd8f94f20645f5a71d7e44878d0efc4f0900
sdegutis <sbdegutis@gmail.com>
2025-02-27 13:00:44 -0600

    Writer file.

---
+++ site/apps/writer.ts
@@ -0,0 +1,32 @@
+import { Scroll } from "../sys32/containers/scroll.js";
+import { TextArea } from "../sys32/containers/textarea.js";
+import type { System } from "../sys32/core/system.js";
+import { View } from "../sys32/core/view.js";
+import { Panel } from "../sys32/desktop/panel.js";
+import { makeVacuumLayout } from "../sys32/util/layouts.js";
+
+export default (sys: System) => {
+
+  const { $ } = sys;
+
+  const textarea = $(TextArea, { background: 0x00990033 });
+
+  textarea.text = 'foo\nbar\n\nhello world'
+
+  textarea.colors[10] = 0x0000ffff;
+  textarea.colors[5] = 0xffff0099;
+  // textarea.colors.length = 0
+
+
+  const panel = $(Panel, { title: 'text test', w: 170, h: 150, },
+    $(View, { layout: makeVacuumLayout(), background: 0x44444433 },
+      $(Scroll, { background: 0x0000ff11 },
+        textarea
+      )
+    )
+  );
+  sys.root.addChild(panel);
+  panel.layoutTree();
+  panel.focus();
+
+};
--- site/main.ts
+++ site/main.ts
@@ -2,6 +2,7 @@ import demo from "./apps/demo.js";
 import filer from "./apps/filer.js";
 import mapmaker from "./apps/mapmaker.js";
 import painter from "./apps/painter.js";
+import writer from "./apps/writer.js";
 import { Scroll } from "./sys32/containers/scroll.js";
 import { TextArea } from "./sys32/containers/textarea.js";
 import { System } from "./sys32/core/system.js";
@@ -18,7 +19,8 @@ sys.crt.autoscale();
 // sys.root.background = 0x003300ff;
 // sys.root.layout = makeVacuumLayout()
 
-mapmaker(sys);
+writer(sys);
+// mapmaker(sys);
 // filer(sys)
 // demo(sys)
 // painter(sys);
@@ -31,28 +33,3 @@ ws.addProgram('filer', filer);
 ws.addProgram('mapmaker', mapmaker);
 ws.addProgram('painter', painter);
 ws.addProgram('writer', writer);
-
-
-const { $ } = sys;
-
-function writer(sys: System) {
-  const textarea = $(TextArea, { background: 0x00990033 });
-
-  textarea.text = 'foo\nbar\n\nhello world'
-
-  textarea.colors[10] = 0x0000ffff;
-  textarea.colors[5] = 0xffff0099;
-  // textarea.colors.length = 0
-
-
-  const panel = $(Panel, { title: 'text test', w: 170, h: 150, },
-    $(View, { layout: makeVacuumLayout(), background: 0x44444433 },
-      $(Scroll, { background: 0x0000ff11 },
-        textarea
-      )
-    )
-  );
-  sys.root.addChild(panel);
-  panel.layoutTree();
-  panel.focus();
-}
