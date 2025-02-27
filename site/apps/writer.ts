d09e8f49365c6be10ff86225d2a544ed15695909
sdegutis <sbdegutis@gmail.com>
2025-02-27 13:11:42 -0600

    Thinking...

--- site/apps/writer.ts
+++ site/apps/writer.ts
@@ -12,21 +12,18 @@ export default (sys: System) => {
   const textarea = $(TextArea, { background: 0x00990033 });
 
   textarea.text = 'foo\nbar\n\nhello world'
-
   textarea.colors[10] = 0x0000ffff;
   textarea.colors[5] = 0xffff0099;
   // textarea.colors.length = 0
 
-
-  const panel = $(Panel, { title: 'text test', w: 170, h: 150, },
+  const panel = $(Panel, { title: 'writer', w: 70, h: 50, },
     $(View, { layout: makeVacuumLayout(), background: 0x44444433 },
       $(Scroll, { background: 0x0000ff11 },
         textarea
       )
     )
   );
+
   sys.root.addChild(panel);
-  panel.layoutTree();
-  panel.focus();
 
 };
