f96c5354c2eb184f9cc1773db675a6c7acc9441a
sdegutis <sbdegutis@gmail.com>
2025-03-01 10:47:24 -0600

    Better db fn.

--- site/sys32/core/fs.ts
+++ site/sys32/core/fs.ts
@@ -309,6 +309,31 @@ type DbFile = {
   content?: string,
 };
 
+
+async function opendb<T extends { key: string }>(dbname: string) {
+  const db = await new Promise<IDBDatabase>(resolve => {
+    const r = window.indexedDB.open(dbname, 1);
+    r.onupgradeneeded = () => { r.result.createObjectStore('kvs', { keyPath: 'key' }); };
+    r.onsuccess = () => { resolve(r.result); };
+  });
+
+  async function run<U>(mode: IDBTransactionMode, fn: (store: IDBObjectStore) => IDBRequest<U>) {
+    const p = Promise.withResolvers<U>();
+    const t = db.transaction('kvs', mode);
+    const r = fn(t.objectStore('kvs'));
+    r.onsuccess = () => p.resolve(r.result);
+    return p.promise;
+  }
+
+  return {
+    all: async () => run<T[]>('readonly', store => store.getAll()),
+    set: async (val: T) => run('readwrite', store => store.put(val)),
+    get: async (key: string) => run<T>('readonly', store => store.get(key)),
+    del: async (key: string) => run('readwrite', store => store.delete(key)),
+  };
+}
+
+
 const db = await new Promise<IDBDatabase>(resolve => {
   const r = window.indexedDB.open('fs', 1);
   r.onerror = console.error;
