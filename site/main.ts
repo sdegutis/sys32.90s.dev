87c9aaf8ba94e94aa2d5a5175b82ef74d124eccc
sdegutis <sbdegutis@gmail.com>
2025-03-03 06:58:10 -0600

    Readd demo.

--- site/main.ts
+++ site/main.ts
@@ -6,9 +6,10 @@ sys.init(document.querySelector('canvas')!);
 crt.autoscale();
 
 await ws.addProgram('filer', import.meta.resolve('./apps/filer/'));
+await ws.addProgram('demo', import.meta.resolve('./apps/demo/'));
 await ws.addProgram('mapmaker', import.meta.resolve('./apps/mapmaker/'));
 await ws.addProgram('painter', import.meta.resolve('./apps/painter/'));
 await ws.addProgram('writer', import.meta.resolve('./apps/writer/'));
 await ws.addProgram('fontmaker', import.meta.resolve('./apps/fontmaker/'));
 
-ws.launch('filer')
+ws.launch('demo')
