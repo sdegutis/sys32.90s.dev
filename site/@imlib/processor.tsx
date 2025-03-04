038498f285531afd67bfc015087d5028e6cb3bd0
sdegutis <sbdegutis@gmail.com>
2025-03-04 16:08:47 -0600

    Icon.

--- site/@imlib/processor.tsx
+++ site/@imlib/processor.tsx
@@ -4,26 +4,36 @@ const copyright = `Copyright ©️ ${new Date().getFullYear()} Novo Cantico LLC.
 
 const $ = jsxToString;
 
+const icon = <link
+  rel="shortcut icon"
+  href={`data:image/svg+xml,${encodeURIComponent(jsxToString(
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+      <path d="M10 2 L10 30 24 16 Z" fill="#19f" />
+    </svg>
+  ))}`}
+/>;
+
 export default (({ inFiles, outFiles }) => {
   const files = [...inFiles];
 
-  const preloadfetch = (files
+  const datas = (files
     .map(f => f.path)
     .filter(s => s.startsWith('/os/data/'))
     .toSpliced(0, 0, '/os/fs/data.json')
-    .map(s => `  ${$(<link rel="preload" as="fetch" href={s} crossOrigin="anonymous" />)}`)
-    .join('\n'));
+    .map(s => <link rel="preload" as="fetch" href={s} crossOrigin="anonymous" />));
 
-  const preload = (files
+  const modules = (files
     .map(f => f.path)
     .filter(s => s.endsWith('.js'))
     .filter(s => !s.includes('@imlib'))
     .filter(s => !s.includes('.json.'))
-    .map(s => `  ${$(<link rel="modulepreload" href={s} />)}`)
-    .join('\n'));
+    .map(s => $(<link rel="modulepreload" href={s} />)));
+
+  const headers = [...datas, ...modules, icon];
+  const toinsert = headers.map(s => `  ${$(s)}`).join('\n');
 
   function insert(s: string) {
-    return s.replace('<head>', `<head>\n${preload}\n${preloadfetch}\n`);
+    return s.replace('<head>', `<head>\n${toinsert}\n`);
   }
 
   for (const file of files) {
