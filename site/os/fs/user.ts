4f2eb72a50bd40fd7b0537dabf1c7bee15a2d81a
sdegutis <sbdegutis@gmail.com>
2025-03-05 07:58:06 -0600

    await in init

--- site/os/fs/user.ts
+++ site/os/fs/user.ts
@@ -1,17 +1,17 @@
 import { opendb } from "./db.js";
 import type { Drive, DriveItem, DriveNotificationType } from "./drive.js";
 
-const db = await opendb<{ path: string, content?: string }>('idbfs', 'path');
-
 export class UserDrive implements Drive {
 
+  db!: Awaited<ReturnType<typeof opendb<{ path: string, content?: string }>>>;
   items = new Map<string, DriveItem>();
   notify?: (type: DriveNotificationType, path: string) => void;
 
   async mount(notify: (type: DriveNotificationType, path: string) => void) {
     this.notify = notify;
+    this.db = await opendb<{ path: string, content?: string }>('idbfs', 'path');
 
-    for (const { path, content } of await db.all()) {
+    for (const { path, content } of await this.db.all()) {
       if (path.endsWith('/')) {
         this.items.set(path, { type: 'folder' });
       }
@@ -23,14 +23,14 @@ export class UserDrive implements Drive {
 
   async putdir(path: string) {
     this.items.set(path, { type: 'folder' });
-    db.set({ path });
+    this.db.set({ path });
     this.notify?.('modified', path);
   }
 
   async putfile(path: string, content: string) {
     const has = this.items.has(path);
     this.items.set(path, { type: 'file', content });
-    db.set({ path, content });
+    this.db.set({ path, content });
     this.notify?.(has ? 'modified' : 'appeared', path);
   }
 
@@ -38,7 +38,7 @@ export class UserDrive implements Drive {
     for (const key of this.items.keys()) {
       if (key.startsWith(path)) {
         this.items.delete(key);
-        db.del(key);
+        this.db.del(key);
       }
     }
     this.notify?.('disappeared', path);
@@ -46,7 +46,7 @@ export class UserDrive implements Drive {
 
   async rmfile(path: string) {
     this.items.delete(path);
-    db.del(path);
+    this.db.del(path);
     this.notify?.('disappeared', path);
   }
 
