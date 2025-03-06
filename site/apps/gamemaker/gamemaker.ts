fdbf0e1e0a6edae370d10d03aa8902d4b50c46b8
sdegutis <sbdegutis@gmail.com>
2025-03-06 17:39:02 -0600

    New app.

---
+++ site/apps/gamemaker/gamemaker.ts
@@ -0,0 +1,92 @@
+import { TextArea } from "../../os/containers/textarea.js";
+import { makeDynamic } from "../../os/core/dyn.js";
+import { Panel } from "../../os/core/panel.js";
+import { sys } from "../../os/core/system.js";
+import { $ } from "../../os/core/view.js";
+import { fs } from "../../os/fs/fs.js";
+import { showPrompt } from "../../os/util/dialog.js";
+import { Reactive } from "../../os/util/events.js";
+import { showMenu } from "../../os/util/menu.js";
+
+function makeFilePanel(opts: {
+  panel: Panel;
+  filepath: string | undefined;
+  loadData(s: string): void;
+  saveData(): string;
+}) {
+  const panel = opts.panel;
+  const title = panel.title;
+  const file = makeDynamic({ path: opts.filepath });
+
+  panel.onMenu = function doMenu() {
+    showMenu([
+      { text: 'load', onClick: loadFile },
+      { text: 'save', onClick: saveFile },
+    ])
+  };
+
+  file.$path.watch(s => {
+    panel.title = !s ? `${title}:[no file]` : `${title}:${s}`;
+    panel.layoutTree();
+  });
+
+  if (file.path) {
+    const s = fs.get(file.path);
+    if (s) {
+      opts.loadData(s);
+    }
+  }
+
+  async function loadFile() {
+    const s = await showPrompt('file path?');
+    if (!s) return;
+    file.path = s;
+
+    const data = fs.get(file.path);
+    if (data) {
+      opts.loadData(data);
+    }
+  }
+
+  async function saveFile() {
+    if (!file.path) {
+      const s = await showPrompt('file path?');
+      if (!s) return;
+      file.path = s;
+    }
+    fs.put(file.path, opts.saveData());
+  }
+
+  panel.onKeyDown = (key) => {
+    if (key === 'o' && sys.keys['Control']) {
+      loadFile();
+      return true;
+    }
+    else if (key === 's' && sys.keys['Control']) {
+      saveFile();
+      return true;
+    }
+    return false;
+  };
+
+}
+
+export default (filepath?: string) => {
+
+  const textarea = $(TextArea, { background: 0x00007777 });
+  const panel = $(Panel, { title: 'writer', w: 120, h: 100, }, textarea);
+
+  textarea.text = ''
+
+  makeFilePanel({
+    filepath,
+    panel,
+    loadData: s => textarea.text = s,
+    saveData: () => textarea.text,
+  });
+
+  panel.show();
+
+  textarea.focus();
+
+};
--- site/main.ts
+++ site/main.ts
@@ -11,9 +11,9 @@ await ws.addProgram('filer', import.meta.resolve('./apps/filer/'));
 await ws.addProgram('settings', import.meta.resolve('./apps/settings/'));
 await ws.addProgram('demo', import.meta.resolve('./apps/demo/'));
 await ws.addProgram('mapmaker', import.meta.resolve('./apps/mapmaker/'));
+await ws.addProgram('gamemaker', import.meta.resolve('./apps/gamemaker/'));
 await ws.addProgram('painter', import.meta.resolve('./apps/painter/'));
 await ws.addProgram('writer', import.meta.resolve('./apps/writer/'));
 await ws.addProgram('fontmaker', import.meta.resolve('./apps/fontmaker/'));
 
-// ws.launch('writer', 'os/controls/button.ts')
-// ws.launch('settings')
+ws.launch('gamemaker')
