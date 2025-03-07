2b82c1a550f5539534890919b13480cdb4049589
sdegutis <sbdegutis@gmail.com>
2025-03-07 13:07:44 -0600

    Hmm.

--- site/gamemaker/main.ts
+++ site/gamemaker/main.ts
@@ -22,7 +22,7 @@ const highlightings: Record<string, [number, RegExp]> = {
   keyw: [0xff00ffcc, /(export|function|let|const)/g],
   punc: [0xffffff77, /([(){}=,])/g],
   call: [0x0099ffff, /([a-zA-Z.]+)\(/g],
-  spcl: [0xcccc00ff, new RegExp(`(${Object.keys(api).reverse().join('|')})`, 'g')],
+  spcl: [0xcccc00ff, new RegExp(`(${[...Object.keys(api).reverse(), 'draw', 'tick'].join('|')})`, 'g')],
   nums: [0x00ffffff, /(0x[0-9a-fA-F]+|[0-9.]+)/g],
 }
 
