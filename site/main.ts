8441f25489a2d8a56752bfbbfd0c5934073b065f
sdegutis <sbdegutis@gmail.com>
2025-03-07 07:41:53 -0600

    One.

--- site/main.ts
+++ site/main.ts
@@ -4,7 +4,6 @@ import { ws } from "./os/desktop/workspace.js"
 await ws.addProgram("filer", import.meta.resolve("./apps/filer/"))
 await ws.addProgram("settings", import.meta.resolve("./apps/settings/"))
 await ws.addProgram("mapmaker", import.meta.resolve("./apps/mapmaker/"))
-await ws.addProgram("gamemaker", import.meta.resolve("./apps/gamemaker/"))
 await ws.addProgram("painter", import.meta.resolve("./apps/painter/"))
 await ws.addProgram("writer", import.meta.resolve("./apps/writer/"))
 await ws.addProgram("fontmaker", import.meta.resolve("./apps/fontmaker/"))
