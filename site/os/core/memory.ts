bddd4fbc4291f6ba251f456219bf28a45352d9c8
sdegutis <sbdegutis@gmail.com>
2025-03-07 04:37:24 -0600

    Didn't work anyway.

--- site/os/core/memory.ts
+++ site/os/core/memory.ts
@@ -1,26 +1,10 @@
 import { fs } from "../fs/fs.js";
-import { Reactive } from "../util/events.js";
 import { Bitmap } from "./bitmap.js";
 import { Cursor } from "./cursor.js";
-import { makeDynamic } from "./dyn.js";
 import { Font } from "./font.js";
-import { sys } from "./system.js";
 
-function livefile<T>(path: string, to: (content: string) => T) {
-  const s = fs.get(path)!;
-  const r = new Reactive<T>(to(s));
-  fs.watchTree(path, (type) => {
-    if (type === 'disappeared') return;
-    const s = fs.get(path)!;
-    r.update(to(s));
-    sys.needsRedraw = true;
-    sys.layoutTree();
-  });
-  return r;
-}
-
-export const mem = makeDynamic({
-  $font: livefile('sys/font1.font', s => new Font(s)),
-  $pointer: livefile('sys/pointer.bitmap', s => Cursor.fromBitmap(Bitmap.fromString(s))),
-  $menubuttonImage: livefile('sys/menubutton.bitmap', s => Bitmap.fromString(s)),
-});
+export const mem = {
+  font: new Font(fs.get('sys/font1.font')!),
+  pointer: Cursor.fromBitmap(Bitmap.fromString(fs.get('sys/pointer.bitmap')!)),
+  menubuttonImage: Bitmap.fromString(fs.get('sys/menubutton.bitmap')!),
+};
