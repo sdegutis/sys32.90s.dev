be82aecd2fa9d2564ae37f841ea94c533c5278dd
sdegutis <sbdegutis@gmail.com>
2025-03-07 06:42:42 -0600

    Starting...

---
+++ site/gamemaker/main.ts
@@ -0,0 +1,74 @@
+import { SplitX } from "../os/containers/split.js";
+import { TextArea } from "../os/containers/textarea.js";
+import { sys } from "../os/core/system.js";
+import { $, View } from "../os/core/view.js";
+import { Panel } from "../os/desktop/panel.js";
+import { makeVacuumLayout } from "../os/util/layouts.js";
+
+export default function gamemaker() {
+
+  const textarea = $(TextArea, { background: 0x000077ff });
+  const editorView = $(View, {
+    layout: makeVacuumLayout(),
+    onKeyDown: key => {
+      if (key === 'r' && sys.keys['Control']) { runGame(); return true; }
+      if (key === 'Escape') { stopGame(); return true; }
+      return false;
+    },
+  },
+    textarea
+  );
+
+  const panel = $(SplitX, { pos: 320 / 2 },
+    editorView
+  );
+
+  let _draw: (() => void) | undefined;
+  function draw() {
+    _draw?.();
+  }
+
+  const gameView = $(View, { background: 0x000000ff, cursor: null, draw });
+
+  let gametick: (() => void) | undefined;
+
+  let running = false;
+
+  async function runGame() {
+    if (running) return;
+
+    editorView.parent!.children = [gameView];
+
+    running = true;
+
+    const prelude = `import {crt} from '${window.origin}/os/core/crt.js';\n`;
+    const blob = new Blob([prelude + textarea.text], { type: 'application/javascript' });
+    const url = URL.createObjectURL(blob);
+    const mod = await import(url);
+
+    _draw = mod.draw;
+
+    if (typeof mod.tick === 'function') {
+      gametick = sys.onTick.watch(mod.tick);
+    }
+  }
+
+  function stopGame() {
+    if (!running) return;
+    _draw = undefined;
+    gametick?.();
+    gameView.parent!.children = [editorView];
+    running = false;
+  }
+
+  textarea.text = `
+export function draw() {
+  crt.rectFill(0,0,20,20,0x99000099)
+}
+`.trimStart();
+
+  sys.root.layout = makeVacuumLayout();
+  sys.root.children = [panel];
+  sys.layoutTree();
+  sys.focus(textarea);
+};
--- site/main.ts
+++ site/main.ts
@@ -1,3 +1,4 @@
+import gamemaker from "./gamemaker/main.js";
 import { ws } from "./os/desktop/workspace.js";
 
 await ws.addProgram('filer', import.meta.resolve('./apps/filer/'));
@@ -8,6 +9,8 @@ await ws.addProgram('painter', import.meta.resolve('./apps/painter/'));
 await ws.addProgram('writer', import.meta.resolve('./apps/writer/'));
 await ws.addProgram('fontmaker', import.meta.resolve('./apps/fontmaker/'));
 
-ws.showDesktop();
+// ws.showDesktop();
 
-ws.launch('gamemaker')
+// ws.launch('gamemaker')
+
+gamemaker();
