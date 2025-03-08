277cd37efe8ca6fb3e8cac75b629a32bd9c6a482
sdegutis <sbdegutis@gmail.com>
2025-03-07 20:13:11 -0600

    Experiment.

--- site/gamemaker/gfxtab.ts
+++ site/gamemaker/gfxtab.ts
@@ -1,8 +1,67 @@
-import { View } from "../os/core/view.js"
+import { Border } from "../os/containers/border.js"
+import { SplitY } from "../os/containers/split.js"
+import { $, View } from "../os/core/view.js"
+import { centerLayout, vacuumAllLayout } from "../os/util/layouts.js"
+
+class GridX extends View {
+
+  cols = 10
+  gap = 0
+
+  private get rows() { return Math.ceil(this.children.length / this.cols) }
+
+  override adjust(): void {
+    const c = this.firstChild!
+    const rows = this.rows
+    const cols = this.cols
+    this.w = cols * c.w + this.gap * (cols - 1)
+    this.h = rows * c.h + this.gap * (rows - 1)
+  }
+
+  override layout(): void {
+    const c = this.firstChild!
+    const rows = this.rows
+    const cols = this.cols
+
+    for (let y = 0; y < rows; y++) {
+      for (let x = 0; x < cols; x++) {
+        const i = y * cols + x
+        const child = this.children[i]
+        if (!child) return
+        child.x = x * c.w + this.gap * x
+        child.y = y * c.h + this.gap * y
+      }
+    }
+  }
+
+}
 
 export class SpriteEditor extends View {
 
   override background = 0x000000ff
+
+  override layout = vacuumAllLayout
+
+  override init(): void {
+    this.children = [
+      $(SplitY, { pos: 70, resizable: true },
+        $(View, { background: 0x000000ff, layout: centerLayout },
+          $(Border, { background: 0xffffff22, padding: 1 },
+            $(GridX, { cols: 4, gap: 1, },
+              ...palettes.vinik24.map(n =>
+                $(View, { w: 7, h: 7, background: n })
+              )
+            )
+          )
+        ),
+        $(SplitY, { pos: 30, resizable: true },
+          $(View, { background: 0x222222ff }),
+          $(View, { background: 0x333333ff }),
+        ),
+      )
+    ]
+    console.log(this.children)
+  }
 
 }
 
