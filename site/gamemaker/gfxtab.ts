49506870039ca762db1f6951660af999e5e09845
sdegutis <sbdegutis@gmail.com>
2025-03-08 06:50:46 -0600

    Grid file.

--- site/gamemaker/gfxtab.ts
+++ site/gamemaker/gfxtab.ts
@@ -1,4 +1,5 @@
 import { Border } from "../os/containers/border.js"
+import { GridX } from "../os/containers/grid.js"
 import { SplitY } from "../os/containers/split.js"
 import { $, View } from "../os/core/view.js"
 import { centerLayout, vacuumAllLayout } from "../os/util/layouts.js"
@@ -31,39 +32,6 @@ export class SpriteEditor extends View {
 
 }
 
-
-class GridX extends View {
-
-  cols = 10
-  gap = 0
-
-  private get rows() { return Math.ceil(this.children.length / this.cols) }
-
-  override adjust(): void {
-    const c = this.firstChild!
-    const rows = this.rows
-    const cols = this.cols
-    this.w = cols * c.w + this.gap * (cols - 1)
-    this.h = rows * c.h + this.gap * (rows - 1)
-  }
-
-  override layout(): void {
-    const c = this.firstChild!
-    const rows = this.rows
-    const cols = this.cols
-
-    for (let y = 0; y < rows; y++) {
-      for (let x = 0; x < cols; x++) {
-        const i = y * cols + x
-        const child = this.children[i]
-        if (!child) return
-        child.x = x * c.w + this.gap * x
-        child.y = y * c.h + this.gap * y
-      }
-    }
-  }
-
-}
 
 const palettes = {
 
