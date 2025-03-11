02d7e00b958110bf68d1f5fb7ba464c47ffd25e3
sdegutis <sbdegutis@gmail.com>
2025-03-11 18:39:00 -0500

    Fix.

--- site/os/util/menu.ts
+++ site/os/util/menu.ts
@@ -9,6 +9,20 @@ import { $ } from "./dyn.js"
 
 export type MenuItem = '-' | { text: string, onClick: () => void }
 
+class MenuDivider extends View {
+
+  override onResized(): void { }
+
+  override layout(): void {
+    this.w = this.parent?.w ?? 20
+  }
+
+  override draw(): void {
+    crt.rectFill(0, 2, this.w, 1, 0xffffff11)
+  }
+
+}
+
 export function showMenu(items: MenuItem[], adjust?: (menu: View) => void) {
   const menu = $(Border, {
     x: sys.mouse.x,
@@ -21,12 +35,8 @@ export function showMenu(items: MenuItem[], adjust?: (menu: View) => void) {
   },
     $(GroupY, { align: 'a' },
       ...items.map(it => it === '-' ?
-        $(View, { h: 5, draw() { crt.rectFill(0, 2, this.w, 1, 0xffffff11) } }) :
-        $(Button, {
-          padding: 2, onClick: () => {
-            it.onClick()
-          }
-        },
+        $(MenuDivider, { h: 5, w: 10 }) :
+        $(Button, { padding: 2, onClick: () => { it.onClick() } },
           $(Label, { text: it.text })
         )
       )
