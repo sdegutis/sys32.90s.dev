a3d30ec8051979fd10bc382debf5918f2e5cf669
sdegutis <sbdegutis@gmail.com>
2025-03-11 18:59:36 -0500

    Not now.

--- site/@imlib/processor.tsx
+++ site/@imlib/processor.tsx
@@ -1,6 +1,4 @@
 import { jsxToString, processFile, type SiteProcessor } from "@imlib/core"
-import path from 'path'
-const pkg = require(path.join(process.cwd(), 'package.json'))
 
 const $ = jsxToString
 
@@ -31,14 +29,8 @@ export default (({ inFiles, outFiles }) => {
     .filter(s => ext(s) === 'js')
     .map(s => <link rel="modulepreload" href={s} />))
 
-  const importmap = $(<script type="importmap">{JSON.stringify({
-    "imports": Object.fromEntries(Object.keys(pkg.dependencies).map(name =>
-      [name, `https://cdn.jsdelivr.net/npm/${name}@latest/+esm`]
-    ))
-  })}</script>)
-
   const iconlink = <link rel="shortcut icon" href={`data:image/svg+xml,${encodeURIComponent(icon)}`} />
-  const headers = [...datas, ...modules, iconlink, importmap]
+  const headers = [...datas, ...modules, iconlink]
   const toinsert = headers.map(s => `  ${$(s)}`).join('\n')
 
   function insert(s: string) {
