74937e83b92297b02febb7dc530fa8ebd04ab44f
sdegutis <sbdegutis@gmail.com>
2025-03-14 14:38:57 -0500

    Only used here.

--- site/os/fs/mountfs.ts
+++ site/os/fs/mountfs.ts
@@ -160,3 +160,19 @@ export class MountedDrive implements Drive {
   }
 
 }
+
+type FileSystemObserverRecord =
+  | { root: FileSystemDirectoryHandle, relativePathComponents: string[], type: 'appeared', changedHandle: FileSystemFileHandle | FileSystemDirectoryHandle }
+  | { root: FileSystemDirectoryHandle, relativePathComponents: string[], type: 'disappeared', changedHandle: null }
+  | { root: FileSystemDirectoryHandle, relativePathComponents: string[], type: 'modified', changedHandle: FileSystemFileHandle | FileSystemDirectoryHandle }
+  | { root: FileSystemDirectoryHandle, relativePathComponents: string[], type: 'moved', changedHandle: FileSystemFileHandle | FileSystemDirectoryHandle, relativePathMovedFrom: string[] }
+  | { root: FileSystemDirectoryHandle, relativePathComponents: string[], type: 'unknown', changedHandle: null }
+  | { root: FileSystemDirectoryHandle, relativePathComponents: string[], type: 'errored', changedHandle: null }
+
+declare class FileSystemObserver {
+  constructor(callback: (records: FileSystemObserverRecord[], observer: FileSystemObserver) => void)
+  observe(fileHandle: FileSystemHandle): void
+  observe(fileHandle: FileSystemDirectoryHandle, options?: { recursive: boolean }): void
+  private unobserve(fileHandle: FileSystemHandle): void
+  disconnect(): void
+}
